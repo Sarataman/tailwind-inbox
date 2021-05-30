e4fa44b501aab47428acf31b3deaad33a63a16bf
Sarah <sarahehi.sa@gmail.com>
2021-05-30 16:53:06 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app" class="min-h-screen bg-gray-200 antialiased xl:flex xl:flex-col xl:h-screen">
+  </div>
+</template>
+
+<script>
+
+export default {
+  // name: 'app',
+  // },
+  // data() {
+  //   return {
+  //   }
+  // }
+}
+</script>
+
+<style src="./assets/css/tailwind.css"></style>
